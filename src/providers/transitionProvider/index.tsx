af528ac8fa74c61bde74312c5c1d15e1dbd53dd0
Gbmesquita-costa <mesquitag313@gmail.com>
2024-05-30 12:49:19 -0300

    analytics

--- src/providers/transitionProvider/index.tsx
+++ src/providers/transitionProvider/index.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { AnimatePresence, motion } from "framer-motion";
 import { PageTransition } from "@/components/pageTransition";
 
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "sonner";
 
 interface TransitionProviderProps {
@@ -16,16 +17,20 @@ export function TransitionProvider({ children }: TransitionProviderProps): JSX.E
     const router = usePathname()
 
     return (
-        <AnimatePresence mode="wait">
-            <motion.div key={router} className="h-full">
-                <PageTransition />
-                <Toaster
-                    richColors={true}
-                    duration={2000}
-                    position="top-right"
-                />
-                {children}
-            </motion.div>
-        </AnimatePresence>
+        <>
+            <AnimatePresence mode="wait">
+                <motion.div key={router} className="h-full">
+                    <PageTransition />
+                    <Toaster
+                        richColors={true}
+                        duration={2000}
+                        position="top-right"
+                    />
+                    {children}
+                </motion.div>
+            </AnimatePresence>
+
+            <Analytics />
+        </>
     )
 }
